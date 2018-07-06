fixed average q-value plots

src/QLearning/classicControl.py
@@ -36,7 +36,8 @@ class ClassicControl:
         m = max(self.nBuckets)
         return (m - 1) * sum([m**i for i in range(len(self.nBuckets))]) + 1
 
-    def learn(self, plot = False, exp = ''):
+    def learn(self, plot = False):
+    	self.plotting = plot
         avgQ = [0 for _ in range(self.nEpisodes)]
         rewards = [0 for _ in range(self.nEpisodes)]
         episodes = [i for i in range(0, self.nEpisodes)]
@@ -47,18 +48,20 @@ class ClassicControl:
 
             # Record results for plotting
             if plot:
-                avgQ[episode] = np.sum(self.Q)/(np.prod(self.nBuckets * self.aSize))
+                avgQ[episode] = np.sum(self.Q)/(np.prod(self.nBuckets) * self.aSize)
                 rewards[episode] = reward
 
         if plot:
-            self.plot(episodes, avgQ, "Average Q Value", exp)
-            self.plot(episodes, rewards, "Rewards", exp)
+            self.plot(episodes, avgQ, "Average Q Value")
+            self.plot(episodes, rewards, "Rewards")
         return
 
     def runEpisode(self):
+        done = False
         observation = self.env.reset()
         state = self.preprocess(observation)
-        for time in range(self.maxEpisodeLength):
+        while not done:
+            self.trainingTime += 1
             action = eGreedy(self.Q[state], self.getExploreRate())
             newObservation, reward, done, info = self.env.step(action)
             newState = self.preprocess(newObservation)
@@ -69,13 +72,14 @@ class ClassicControl:
             state = newState
             if done: break
             self.trainingTime += 1
-        return self.test(visualise = False)
+        if self.plotting: return self.test(visualise = False)
+        return 0
 
     #def getExploreRate(self): return max(self.minExploreRate, min(1, 5 - math.log10(self.trainingTime + 1)))
     def getExploreRate(self): return self.minExploreRate
 
-    #def getLearningRate(self): return max(self.minLearningRate, min(1, 5 - math.log10(self.trainingTime + 1)))
-    def getLearningRate(self): return self.minLearningRate
+    def getLearningRate(self): return max(self.minLearningRate, min(1, 5 - math.log10(self.trainingTime + 1)))
+    # def getLearningRate(self): return self.minLearningRate
 
 
     def test(self, testEpisodes = 10, testMaxTime = 250, visualise = True):
@@ -110,13 +114,13 @@ class ClassicControl:
         n = len(buckets)
         return int(sum([m**j * buckets[j] for j in range(n)]))
     
-    def plot(self, x, y, title, exp):
+    def plot(self, x, y, title):
         plt.figure()
         plt.plot(x, y)
         plt.xlabel("Episode")
         plt.ylabel(title)
         plt.title(title + " for " + self.game)
-        plt.savefig(title.replace(" ", "") + exp + ".png")
+        plt.savefig(title.replace(" ", "") + ".png")
 
 def eGreedy(q, epsilon):
     aSize = len(q)
@@ -142,15 +146,14 @@ def getBucket(value, lowerB, upperB, nBuckets):
 
 def pause(): programPause = raw_input("Press the <ENTER> key to continue...")
 
-
 # pole = ClassicControl(game = 'CartPole-v0', bounds = [[-2.4, 2.4], [-2, 2], [-0.21, 0.21], [-2, 2]], nBuckets = [1, 1, 20, 20], aSize = 2)
 # pole.setLearningParameters(nEpisodes = 500, maxEpisodeLength = 200, discount = 0.99, minLearningRate = 0.05, minExploreRate = 0.1)
-# pole.learn(plot = False)
+# pole.learn(plot = True)
 # pause()
 # pole.test()
 
 car = ClassicControl(game = 'MountainCar-v0', bounds = [[-1.2, 1.2], [-0.07, 0.07]], nBuckets = [10, 30], aSize = 3)
-car.setLearningParameters(nEpisodes = 5000, discount = 0.99, minLearningRate = 0.05, minExploreRate = 0.1)
+car.setLearningParameters(nEpisodes = 500, discount = 0.99, minLearningRate = 0.05, minExploreRate = 0.1)
 car.learn(plot = True)
 pause()
 result = car.test()
a05056f60374b3b7f63b3d3f937ecf2e78affd2d
Patrik Gerber <pgerber@t3420n-1230bl.ipamnet.ipam.ucla.edu>
2018-07-10 13:37:33 -0700

    working version of deepcart

--- src/Cartpole_Deep/deep_Cartpole.py
+++ src/Cartpole_Deep/deep_Cartpole.py
@@ -9,10 +9,15 @@ import subprocess
 env = gym.make('CartPole-v0')
 
 import tensorflow as tf
+sess = tf.Session()
+
+# Parameters
+# learning_rate = 0.001
+# batch_size = 32
 
 # Network Parameters
-n_hidden_1 = 16 # 1st layer number of neurons
-n_hidden_2 = 16 # 2nd layer number of neurons
+n_hidden_1 = 10 # 1st layer number of neurons
+n_hidden_2 = 10 # 2nd layer number of neurons
 num_input = 4 # MNIST data input (img shape: 28*28)
 num_classes = 2 # MNIST total classes (0-9 digits)
 
@@ -37,7 +42,9 @@ def neural_net(x):
     out_layer = tf.matmul(layer_2, weights['out']) + biases['bOut']
     return out_layer
 
-# n x m matrix is of the form [[row_1], ... , [row_n]] where row_i is a m-dimensional vector
+# n x m matrix is equal to [[row_1], ... , [row_n]] where row_i is a m-dimensional vector
+
+
 # X is a n.num_input = n.4 matrix
 X = tf.placeholder("float")
 # If X is n.4 then Q is n.2
@@ -45,7 +52,6 @@ Q = neural_net(X)
 # Y is a n.1 column vector 
 Y = tf.placeholder("float")
 action = tf.placeholder("int32")
-learning_rate = tf.placeholder("float")
 max_Q = tf.reduce_max(Q, axis = 1)
 argmax_Q = tf.argmax(Q, axis = 1)
 
@@ -53,13 +59,12 @@ argmax_Q = tf.argmax(Q, axis = 1)
 
 difference = tf.subtract(Y, tf.gather(Q, action, axis = 1))
 loss = tf.reduce_sum(tf.square(difference))
-# Regularisation
-for weight in weights.values():
-	loss = loss + 0.01*tf.reduce_sum(tf.square(weight))
+# for weight in weights.values():
+# 	loss = loss + 0.01*tf.reduce_sum(tf.square(weight))
 
 # optimizer = tf.train.Grad(learning_rate = 0.1)
-# optimizer = tf.train.AdamOptimizer(learning_rate)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
+optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
 train_op = optimizer.minimize(loss)
 
 
@@ -75,15 +80,10 @@ def eGreedy(state, epsilon):
 	return sess.run(argmax_Q, feed_dict = {X: state})[0] if random.random() > epsilon else int(2*random.random())
 
 def getEpsilon(time, nEpisodes):
-	return 0.5
 	return 1 - time/nEpisodes
 
-def getLearningRate(time, nEpisodes):
-	return 0.001
-	return max(0.1, min(0.25 - 0.1*math.log10(time + 1), 0.005))
-
 def greedy(state):
-	return sess.run(argmax_Q, feed_dict = {X: state})[0]
+	return int(sess.run(argmax_Q, feed_dict = {X: state}))
 
 def pause(): programPause = raw_input("Press the <ENTER> key to continue...")
 
@@ -102,13 +102,13 @@ def test(visualise = False, testEpisodes = 10):
 			totalReward += reward
 	return totalReward / float(testEpisodes)
 	
-nEpisodes = 2
+nEpisodes = 3000
 display_step = 20
 discount = 0.99
 epsilon = 1
 replay_memory = []
 replay_capacity = 1000
-minibatch_size = 2
+minibatch_size = 10
 
 time = 0
 for episode in range(nEpisodes):
@@ -119,14 +119,15 @@ for episode in range(nEpisodes):
 	# state is a 1.4 matrix, of the form [[row]]
 	state = phi(initObservation)
 	done = False
-	if episode%20 == 0: fixed_weights = sess.run(trainable_variables)
+	fixed_weights = sess.run(trainable_variables)
+	# print(fixed_weights)
+	# pause()
 	while not done:
 		a = eGreedy(state, getEpsilon(time, nEpisodes))
 
 		newObservation, reward, done, _ = env.step(a)
 		newState = phi(newObservation)
 		replay_memory.append((state, a, newState, reward))
-		if episode == 0: break
 
 		states = [None] * minibatch_size
 		newStates = [None] * minibatch_size
@@ -146,15 +147,15 @@ for episode in range(nEpisodes):
 		max_q = sess.run(max_Q, feed_dict = dict)
 		y = np.matrix([rewards]).T + discount*np.matrix(max_q).T if not done else reward
 
-		sess.run(train_op, feed_dict = {X: states, Y: y, action : actions, learning_rate: getLearningRate(time, nEpisodes)})
+		sess.run(train_op, feed_dict = {X: states, Y: y, action : actions})
 		state = newState
 		time += 1
-	print(sess.run(trainable_variables))
 	p = (episode + 1)%display_step == 0
 	if p: 
+		# print(episode)
 		temp = test()
 		print(temp)
-		if temp > 100:
+		if temp > 50:
 			test(visualise = True, testEpisodes = 5)
 
 test(visualise = True)

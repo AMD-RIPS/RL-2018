a624ae7fc4d4e0a8573cdb246a5f3f8a805dd7c6
Patrik Gerber <pgerber@t3420n-1230bl.ipamnet.ipam.ucla.edu>
2018-07-10 18:17:03 -0700

    Working version of dqn for cartpole

--- src/Cartpole_Deep/deep_Cartpole.py
+++ src/Cartpole_Deep/deep_Cartpole.py
@@ -11,37 +11,23 @@ env = gym.make('CartPole-v0')
 import tensorflow as tf
 
 # Network Parameters
-n_hidden_1 = 32 # 1st layer number of neurons
-n_hidden_2 = 32 # 2nd layer number of neurons
+n_hidden_1 = 64 # 1st layer number of neurons
+n_hidden_2 = 64 # 2nd layer number of neurons
 num_input = 4 # MNIST data input (img shape: 28*28)
 num_classes = 2 # MNIST total classes (0-9 digits)
 
-weights = {
-    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1], stddev = 10)),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], stddev = 10)),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes], stddev = 10))
-}
-
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1], stddev = 10)),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2], stddev = 10)),
-    'bOut': tf.Variable(tf.random_normal([num_classes], stddev = 10))
-}
-
-# concatenate the list of trainable variables
-trainable_variables = weights.values() + biases.values()
-
-def neural_net(x):
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['bOut']
-    return out_layer
-
-# n x m matrix is of the form [[row_1], ... , [row_n]] where row_i is a m-dimensional vector
+# An n x m matrix is of the form [[row_1], ... , [row_n]] where row_i is a m-dimensional vector
 # X is a n.num_input = n.4 matrix
-X = tf.placeholder("float")
+X = tf.placeholder("float", [None, num_input])
 # If X is n.4 then Q is n.2
-Q = neural_net(X)
+
+# Definition of the neural net
+layer1 = tf.layers.dense(inputs = X, units = n_hidden_1, activation = tf.nn.relu, kernel_initializer = tf.truncated_normal_initializer)
+layer2 = tf.layers.dense(inputs = layer1, units = n_hidden_2, activation = tf.nn.relu, kernel_initializer = tf.truncated_normal_initializer)
+Q = tf.layers.dense(inputs = layer2, units =  num_classes)
+
+trainable_variables = tf.trainable_variables()
+
 # Y is a n.1 column vector 
 Y = tf.placeholder("float")
 action = tf.placeholder("int32")
@@ -49,16 +35,23 @@ learning_rate = tf.placeholder("float")
 max_Q = tf.reduce_max(Q, axis = 1)
 argmax_Q = tf.argmax(Q, axis = 1)
 
-loss = tf.losses.mean_squared_error(Y, tf.gather(Q, action, axis = 1))
+Q_actionval = Q[0][action]
+loss = tf.losses.mean_squared_error(Y, Q_actionval)
+
+# _______________________________________________________________
+# At the same time version
+# Q_actionvals = tf.matmul(Q, tf.transpose(tf.one_hot(indices = action, depth = num_classes)))
+# loss = tf.losses.mean_squared_error(Y, Q_actionvals)
+
+
 # Regularisation
 # for weight in weights.values():
-# 	loss = loss + 0.01*tf.reduce_sum(tf.square(weight))
+# 	loss = loss + 0.01*.reduce_sum(tf.square(weight))
 
-optimizer = tf.train.RMSPropOptimizer(learning_rate = 0.001)
-# optimizer = tf.train.AdamOptimizer(learning_rate = 0.0001)
+# optimizer = tf.train.RMSPropOptimizer(learning_rate)
+# optimizer = tf.train.ProximalGradientDescentOptimizer(learning_rate)
+train_op = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(loss)
 # optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.0001)
-train_op = optimizer.minimize(loss)
-
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
@@ -71,12 +64,14 @@ def phi(x):
 def eGreedy(state, epsilon):
 	return sess.run(argmax_Q, feed_dict = {X: state})[0] if random.random() > epsilon else int(2*random.random())
 
-def getEpsilon(time, nEpisodes):
-	return 0.5 - 0.5*time/nEpisodes
+def getEpsilon(episode, nEpisodes):
+	return 0.5 - 0.5*episode/nEpisodes
 
 def getLearningRate(time, nEpisodes):
-	return 0.0001
-	return max(0.1, min(0.25*(1 - time/nEpisodes), 0.0005))
+	return 0.001
+	# decay = 1 - prevReward/200
+	# return max(0.001, decay**4)
+	# return max(0.1, min(1, 2.5 - math.log10(time + 1)))
 
 def greedy(state):
 	return sess.run(argmax_Q, feed_dict = {X: state})[0]
@@ -101,10 +96,9 @@ def test(visualise = False, testEpisodes = 10):
 nEpisodes = 2000
 display_step = 20
 discount = 0.99
-epsilon = 1
 replay_memory = []
 replay_capacity = 1000
-minibatch_size = 64
+minibatch_size = 16
 
 time = 0
 for episode in range(nEpisodes):
@@ -115,44 +109,68 @@ for episode in range(nEpisodes):
 	# state is a 1.4 matrix, of the form [[row]]
 	state = phi(initObservation)
 	done = False
-	if episode%20 == 0: fixed_weights = sess.run(trainable_variables)
+	fixed_weights = sess.run(trainable_variables)
+	# print(fixed_weights)
 	while not done:
-		a = eGreedy(state, getEpsilon(time, nEpisodes))
+		a = eGreedy(state, getEpsilon(episode, nEpisodes))
 
 		newObservation, reward, done, _ = env.step(a)
 		newState = phi(newObservation)
-		replay_memory.append((state, a, newState, reward))
+		replay_memory.append((state, a, newState, reward, done))
 		# filling up replay memory
-		if episode < 20: break
+		if episode < 2: break
 
-		states = [None] * minibatch_size
-		newStates = [None] * minibatch_size
-		actions = [None] * minibatch_size
-		rewards = [None] * minibatch_size
-		i = 0
+		# _____________________________________________________________________________________
+		# Everything at once version
+		# states = [None] * minibatch_size
+		# newStates = [None] * minibatch_size
+		# actions = [None] * minibatch_size
+		# rewards = [None] * minibatch_size
+		# i = 0
+		# for index in np.random.choice(a = len(replay_memory), size = minibatch_size):
+		# 	temp = replay_memory[index]
+		# 	states[i] = temp[0][0]
+		# 	newStates[i] = temp[2][0]
+		# 	actions[i] = temp[1]
+		# 	rewards[i] = temp[3]
+		# 	i += 1
+
+		# dict = {X: newStates}
+		# # dict.update(zip(trainable_variables, fixed_weights))
+		# max_q = sess.run(max_Q, feed_dict = dict)
+		# y = np.matrix([rewards]).T + discount*np.matrix(max_q).T if not done else reward
+
+		# train_dict = {X: states, Y: y, action : actions, learning_rate: getLearningRate(time, nEpisodes)}
+		# # print(sess.run(loss, train_dict))
+		# sess.run(train_op, feed_dict = train_dict)
+
+		# ___________________________________________________________________________________-
+		# separately version
 		for index in np.random.choice(a = len(replay_memory), size = minibatch_size):
 			temp = replay_memory[index]
-			states[i] = temp[0][0]
-			newStates[i] = temp[2][0]
-			actions[i] = temp[1]
-			rewards[i] = temp[3]
-			i += 1
+			s = temp[0]
+			ns = temp[2]
+			a = temp[1]
+			r = temp[3]
+			d = temp[4]
+			dict = {X: ns}
+			dict.update(zip(trainable_variables, fixed_weights))
+			max_q = sess.run(max_Q, feed_dict = dict)[0]
+			y = r + discount*max_q if not d else r
+			train_dict = {X: s, action: a, Y: [[y]], learning_rate: 0.01}
+			sess.run(train_op, feed_dict = train_dict)
 
-		dict = {X: newStates}
-		dict.update(zip(trainable_variables, fixed_weights))
-		max_q = sess.run(max_Q, feed_dict = dict)
-		y = np.matrix([rewards]).T + discount*np.matrix(max_q).T if not done else reward
-
-		sess.run(train_op, feed_dict = {X: states, Y: y, action : actions, learning_rate: getLearningRate(time, nEpisodes)})
 		state = newState
 		time += 1
+	# print(episode)
 	# print(sess.run(trainable_variables))
-	p = (episode + 1)%display_step == 0
-	if p: 
-		temp = test()
-		print(temp)
-		if temp > 100:
-			test(visualise = True, testEpisodes = 5)
+	score = test(testEpisodes = 10)
+	if score == 200: break
+	print("score = {0}, epsilon = {1}, learning rate = {2}".format(score, getEpsilon(episode, nEpisodes), getLearningRate(time, nEpisodes)))
+	# print(prevReward)
+	# if prevReward > 190: 
+	# 	print(episode)
+	# 	break
 
 test(visualise = True)
 

95aaffa4e3d9d365a5d716f4fdfba629ac4b006c
Elvis Nunez <enunez@t3420a-1228bl.ipamnet.ipam.ucla.edu>
2018-07-12 11:32:32 -0700

    Altered network architecture

--- src/Racing/run_deep_q_car.py
+++ src/Racing/run_deep_q_car.py
@@ -6,5 +6,6 @@ acceleration = [0, .5, 1]
 deceleration = [0, .5, 1]
 print ('---- CarRacing ----')
 cr = deep_q_car.Playground('CarRacing-v0', 2, [24,24], .4, .01, .05, .99, 32, 10000, steering, acceleration, deceleration)
+print '------------------- HERE! ----------------------'
 cr.begin_training(10000)
 cr.test_Q(100)

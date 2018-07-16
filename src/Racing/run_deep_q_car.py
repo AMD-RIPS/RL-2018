3633f7df90ebc49f49abe580ce7d31c842a01ebc
Jiajing Guan <jiajingguan@gmail.com>
2018-07-16 09:19:56 -0700

    Test saver

--- src/Racing/run_deep_q_car.py
+++ src/Racing/run_deep_q_car.py
@@ -6,4 +6,4 @@ acceleration = [0, 1]
 deceleration = [0, 0.8]
 print ('---- CarRacing ----')
 cr = deep_q_car.Playground('CarRacing-v0', 2, [24,24], .4, .01, .05, .99, 32, 10000, steering, acceleration, deceleration)
-cr.begin_training(11000, 4)
+cr.begin_training(1, 4)

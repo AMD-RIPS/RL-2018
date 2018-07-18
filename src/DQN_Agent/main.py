9814d3d8d8e7dec69188011ee179a4b382e60ee4
Elvis Nunez <enunez@t3420n-1250bl.ipamnet.ipam.ucla.edu>
2018-07-18 11:25:21 -0700

    New changes

--- src/DQN_Agent/main.py
+++ src/DQN_Agent/main.py
@@ -6,7 +6,7 @@ import environment as env
 
 # Train Pong
 environment = env.env_dict["CarRacing"]()
-control = agent.DQN_Agent(environment=environment, architecture='conv2', explore_rate='basic', learning_rate='basic')
+control = agent.DQN_Agent(environment=environment, architecture='conv2', explore_rate='basic', learning_rate='basic', skip_frames=4)
 control.set_training_parameters(discount=.99, batch_size=32, memory_capacity=10000, num_episodes=10000)
 control.train()
 print(control.test_Q(10, visualize=True))

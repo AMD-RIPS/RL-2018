f7f95e36b59e9d1f5dff97bad9310dfc150716b2
Jiajing Guan <jiajingguan@gmail.com>
2018-07-23 09:36:56 -0700

    Added capability of reloading saved model

--- src/DQN_Agent/agent_init_reload.py
+++ src/DQN_Agent/agent_init_reload.py
@@ -16,21 +16,31 @@ import time
 from tensorflow.python.saved_model import tag_constants
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+SAVE_PATH = DIR_PATH + '/saved_models/tmp3'
+LOG_PATH = DIR_PATH  + "/logs/tmp3"
 
 class DQN_Agent:
     # architecture, explore_rate and learning_rate are strings, see respective files for definitions
-    def __init__(self, environment, architecture, explore_rate, learning_rate):
+    def __init__(self, environment, architecture, explore_rate, learning_rate, reload_bool):
         self.env = environment
         self.architecture = arch.arch_dict[architecture]
         self.explore_rate = expl.expl_dict[explore_rate]()
         self.learning_rate = lrng.lrng_dict[learning_rate]()
-        self.initialize_tf_variables()
+        if reload_bool:
+            self.reload_tf_variables()
+        else:
+            self.initialize_tf_variables()
 
-    def set_training_parameters(self, discount, batch_size, memory_capacity, num_episodes, learning_rate_drop_frame_limit = 1000000):
+    def set_training_parameters(self, discount, batch_size, memory_capacity, num_episodes, 
+        learning_rate_drop_frame_limit = 100000, test_frequency = 100, save_frequence = 50, 
+        num_q_grid = 1000):
         self.discount = discount
         self.replay_memory = rplm.Replay_Memory(memory_capacity, batch_size)
         self.num_episodes = num_episodes
         self.training_metadata = {'num_episodes': num_episodes, 'frame_limit': learning_rate_drop_frame_limit}
+        self.test_frequency = test_frequency
+        self.save_frequence = save_frequence
+        self.num_q_grid = num_q_grid
 
     def initialize_tf_variables(self):
         # Setting up game specific variables
@@ -45,20 +55,20 @@ class DQN_Agent:
         self.alpha = tf.placeholder(dtype=tf.float32, name='alpha')
         self.training_score = tf.placeholder(dtype=tf.float32, name='training_score')
         self.avg_q = tf.placeholder(dtype=tf.float32, name='avg_q')
+        self.loss_monitor = tf.placeholder(dtype=tf.float32, name='loss_monitor')
 
         self.q_grid_set = tf.Variable(initial_value = tf.random_uniform([1000,4]), dtype = tf.float32, name = "q_grid_set")
         self.q_stored = False
 
         self.epsilon = tf.placeholder(dtype=tf.float32, name='epsilon')
-        self.episode = tf.Variable(initial_value=0,trainable=False, dtype=tf.int16, name='episode')
-        self.frames = tf.Variable(initial_value=0,trainable=False,dtype=tf.int16, name='frames')
+        self.episode = tf.Variable(initial_value=0,trainable=False, name='episode')
+        self.frames = tf.Variable(initial_value=0,trainable=False, name='frames')
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
         self.increment_global_step_op = tf.assign(self.global_step, self.global_step+1, name='increment_global_step_op')
         self.increment_frames_op = tf.assign(self.frames, self.frames+1, name='increment_frames_op')
         self.increment_episode_op = tf.assign(self.episode, self.episode+1, name='increment_episode_op')
 
         # Operations
-        # self.Q_value = tf.py_func(self.architecture, [self.state_tf, self.action_size], tf.float32, name='Q_network')
         self.Q_value = self.architecture(self.state_tf, self.action_size)
         self.Q_argmax = tf.argmax(self.Q_value[0], name='Q_argmax')
         self.Q_amax = tf.reduce_max(self.Q_value[0], name='Q_amax')
@@ -79,21 +89,95 @@ class DQN_Agent:
         self.trainable_variables = tf.trainable_variables()
 
         # Tensorboard setup
-        LOG_PATH = DIR_PATH  + "/logs/tmp2"
         self.writer = tf.summary.FileWriter(LOG_PATH)
         self.writer.add_graph(self.sess.graph)
         training_score = tf.summary.scalar("Training score", self.training_score, collections=None, family=None)
         epsilon = tf.summary.scalar("Epsilon", self.epsilon, collections=None, family=None)
         avg_q = tf.summary.scalar("Average Q-value", self.avg_q, collections=None, family=None)
+        loss_monitor = tf.summary.scalar("Loss", self.loss_monitor, collections=None, family=None)
         self.training_summary = tf.summary.merge([avg_q, epsilon])
         self.test_summary = tf.summary.merge([training_score])
-        subprocess.Popen(['tensorboard', '--logdir', DIR_PATH  + "/logs/tmp2", '--port', '6006'])
+        self.loss_summary = tf.summary.merge([loss_monitor])
+        subprocess.Popen(['tensorboard', '--logdir', LOG_PATH, '--port', '6006'])
 
         # Initialising saver
         self.saver = tf.train.Saver()
 
         # Initialising and finalising
         self.sess.run(tf.global_variables_initializer())
+        # self.sess.graph.finalize()
+
+        self.previous_eps_num = self.sess.run(self.episode)
+        self.previous_fra_num = self.sess.run(self.frames)
+
+
+    def reload_tf_variables(self):
+        tf.reset_default_graph() 
+        self.saver = tf.train.import_meta_graph(SAVE_PATH + '/data.chkp.meta')
+
+        # Setting up game specific variables
+        self.state_size = self.env.state_space_size
+        self.action_size = self.env.action_space_size
+        self.state_shape = self.env.state_shape
+
+        # Tensorflow session setup
+        config = tf.ConfigProto()
+        config.allow_soft_placement = True
+        config.gpu_options.allow_growth = True
+        config.log_device_placement = False
+        self.sess = tf.Session(config=config)
+
+        self.saver.restore(self.sess,tf.train.latest_checkpoint(SAVE_PATH))
+        graph = tf.get_default_graph()
+
+        # Tf placeholders
+        self.state_tf = graph.get_tensor_by_name("state_tf:0")
+        self.action_tf = graph.get_tensor_by_name("action_tf:0")
+        self.y_tf = graph.get_tensor_by_name("y_tf:0")
+        self.alpha = graph.get_tensor_by_name("alpha:0")
+        self.training_score = graph.get_tensor_by_name("training_score:0")
+        self.avg_q = graph.get_tensor_by_name("avg_q:0")
+        self.epsilon = graph.get_tensor_by_name("epsilon:0")
+        self.loss_monitor = graph.get_tensor_by_name("loss_monitor:0")
+
+        self.q_grid_set = graph.get_tensor_by_name("q_grid_set:0")
+        self.q_stored = True
+
+        self.episode = graph.get_tensor_by_name("episode:0")
+        self.frames = graph.get_tensor_by_name("frames:0")
+        self.global_step = graph.get_tensor_by_name("global_step:0")
+        self.increment_global_step_op = graph.get_tensor_by_name("increment_global_step_op:0")
+        self.increment_frames_op = graph.get_tensor_by_name("increment_frames_op:0")
+        self.increment_episode_op = graph.get_tensor_by_name("increment_episode_op:0")
+
+        # Operations
+        self.Q_value = graph.get_tensor_by_name("output/BiasAdd:0")
+        self.Q_argmax = graph.get_tensor_by_name("Q_argmax:0")
+        self.Q_amax = graph.get_tensor_by_name("Q_amax:0")
+        self.Q_value_at_action = graph.get_tensor_by_name("Q_value_at_action:0")
+
+        # # Training related
+        self.loss = graph.get_tensor_by_name("loss:0")
+        self.train_op = graph.get_operation_by_name("train_minimize")
+        self.trainable_variables = tf.trainable_variables()
+        self.fixed_target_weights = self.sess.run(self.trainable_variables)
+
+        self.previous_eps_num = self.sess.run(self.episode)
+        self.previous_fra_num = self.sess.run(self.frames)
+
+        # Tensorboard setup
+        self.writer = tf.summary.FileWriter(LOG_PATH)
+        self.writer.add_graph(self.sess.graph)
+        training_score = graph.get_tensor_by_name('Training_score_1:0')
+        epsilon = graph.get_tensor_by_name('Epsilon_1:0')
+        avg_q = graph.get_tensor_by_name('Average_Q-value:0')
+        loss_monitor = graph.get_tensor_by_name('Loss_1:0')
+        self.training_summary = tf.summary.merge([avg_q, epsilon])
+        self.test_summary = tf.summary.merge([training_score])
+        self.loss_summary = tf.summary.merge([loss_monitor])
+        subprocess.Popen(['tensorboard', '--logdir', LOG_PATH, '--port', '6007'])
+
+        # Initialising and finalising
         # self.sess.graph.finalize()
 
 
@@ -105,10 +189,17 @@ class DQN_Agent:
         feed_dict.update(zip(self.trainable_variables, self.fixed_target_weights))
         
         Q_value_batch = self.sess.run(self.Q_value, feed_dict=feed_dict)
+        Q_value_at_action = np.sum(np.multiply(action_batch, Q_value_batch), axis=1)
         y_batch = reward_batch + self.discount*np.multiply(np.invert(done_batch), np.amax(Q_value_batch, axis=1))
 
+        loss = self.sess.run(self.loss, feed_dict={self.y_tf: y_batch, self.Q_value_at_action: Q_value_at_action})
+
         # Performing one step of optimization
-        self.sess.run([self.train_op, self.increment_global_step_op], feed_dict={self.y_tf: y_batch, self.action_tf: action_batch, self.state_tf: state_batch, self.alpha: alpha})
+        self.sess.run([self.train_op, self.increment_global_step_op], 
+            feed_dict={self.y_tf: y_batch, self.action_tf: action_batch, 
+            self.state_tf: state_batch, self.alpha: alpha})
+        self.writer.add_summary(self.sess.run(self.loss_summary, 
+            feed_dict={self.loss_monitor: loss}), self.sess.run(self.episode))
 
     def get_action(self, state, epsilon):
         # Perorming epsilon-greedy action selection
@@ -126,7 +217,7 @@ class DQN_Agent:
             self.training_metadata['episode'] = self.sess.run(self.episode)
             self.sess.run(self.increment_episode_op)
             start_time = time.time()
-            print('Episode {0}/{1}'.format(self.sess.run(self.episode), self.num_episodes+self.sess.run(self.episode)))
+            print('Episode {0}/{1}'.format(self.sess.run(self.episode), self.num_episodes+self.previous_eps_num))
 
             # Setting up game environment
             state = self.env.reset()
@@ -158,22 +249,24 @@ class DQN_Agent:
                 done = info['true_done']
             
             # Creating q_grid if not yet defined and calculating average q-value
-            if not self.q_stored and self.replay_memory.length() > 5000:
+            if not self.q_stored and self.replay_memory.length() > (self.num_q_grid*5):
                 self.q_stored = True
-                q_grid = self.replay_memory.get_q_grid(1000)
+                q_grid = self.replay_memory.get_q_grid(self.num_q_grid)
                 update = tf.assign(self.q_grid_set, np.asarray(q_grid, dtype = np.float32))
                 self.sess.run(update)
             avg_q = self.estimate_avg_q()
             
             # Saving tensorboard data
-            if (episode % 100 == 0) and (episode != 0):
+            if ((episode+1) % self.test_frequency == 0) and (episode > self.test_frequency*3):
                 score = self.test_Q(num_test_episodes=5, visualize = True)
-                self.writer.add_summary(self.sess.run(self.test_summary, feed_dict={self.training_score: score}), self.sess.run(self.episode)/30)
-            self.writer.add_summary(self.sess.run(self.training_summary, feed_dict={self.avg_q: avg_q, self.epsilon: epsilon}), self.sess.run(episode))
+                self.writer.add_summary(self.sess.run(self.test_summary, 
+                    feed_dict={self.training_score: score}), self.sess.run(self.episode)/self.test_frequency)
+            self.writer.add_summary(self.sess.run(self.training_summary, 
+                feed_dict={self.avg_q: avg_q, self.epsilon: epsilon}), self.sess.run(self.episode))
 
             # Saving model
-            if (episode+1) % 100 == 0:
-                self.saver.save(self.sess, DIR_PATH + '/saved_models/tmp/data.chkp')
+            if (episode+1) % self.save_frequence == 0:
+                self.saver.save(self.sess, SAVE_PATH + '/data.chkp')
 
     def test_Q(self, num_test_episodes=10, visualize=False):
         cum_reward = 0
@@ -197,5 +290,4 @@ class DQN_Agent:
     def estimate_avg_q(self):
         if not self.q_stored:
             return 0
-        num_samples = self.sess.run(tf.shape(self.q_grid_set))[0]
         return np.average(np.amax(self.sess.run(self.Q_value, feed_dict={self.state_tf: self.sess.run(self.q_grid_set)}), axis = 1))
--- src/DQN_Agent/main.py
+++ src/DQN_Agent/main.py
@@ -6,6 +6,6 @@ import environment as env
 
 environment = env.env_dict["Classic_Control"]('CartPole-v0')
 control = agent.DQN_Agent(environment=environment, architecture='basic', explore_rate='atari', learning_rate='basic')
-control.set_training_parameters(discount=.99, batch_size=32, memory_capacity=10000, num_episodes=2000)
+control.set_training_parameters(discount=.99, batch_size=32, memory_capacity=10000, num_episodes=1000)
 control.train()
 print(control.test_Q(10, visualize=True))

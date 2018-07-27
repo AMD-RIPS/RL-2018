32ad9f7bcd0f007cf0a485a8aba4bc045e64c2d9
nicholasmalaya <nicholas.malaya@gmail.com>
2018-07-27 15:26:35 -0500

    [RIPS]: adding simple command line handling to remind me how this thing runs.

--- src/DQN_Agent/main.py
+++ src/DQN_Agent/main.py
@@ -6,6 +6,12 @@ import agent
 import environment as env
 import tensorflow as tf
 
+print sys.argv[:]
+
+if(len(sys.argv) <= 2):
+    print 'Usage: python main.py model_name'
+    exit(1)
+
 environment = env.env_dict["CarRacing"]()
 control = agent.DQN_Agent(environment=environment, architecture='atari', explore_rate='atari', learning_rate='atari', model_name=sys.argv[1])
 control.set_training_parameters(discount=.99, batch_size=32, memory_capacity=10000, num_episodes=100000)

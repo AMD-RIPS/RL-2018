97e62ae07c3071dccaab30521fa463a774de9f28
Jiajing Guan <jiajingguan@gmail.com>
2018-08-01 14:56:53 -0700

    Add score limit for convergence condition

--- src/DQN_Agent/main.py
+++ src/DQN_Agent/main.py
@@ -5,10 +5,14 @@ import sys
 import agent
 import environment as env
 import tensorflow as tf
+import hyperparameters.setups as setups 
 
-environment = env.env_dict["CarRacing"](type = "OneCurve", seed = 2)
-control = agent.DQN_Agent(environment=environment, architecture='nature', explore_rate='decay', learning_rate='atari', model_name=sys.argv[1])
-control.set_training_parameters(discount=.99, batch_size=32, memory_capacity=10000, num_episodes=10000)
-# control.load("/home/jguan/Documents/RIPS/RL-2018/src/DQN_Agent/models/carracing_oneturn_1000frames_accumulatereward_blacktile")
+game = "CarRacing"
+
+environment = env.env_dict[game](type = "OneCurve", seed = 2)
+# environment = env.env_dict[game]()
+control = agent.DQN_Agent(environment=environment, model_name=sys.argv[1], **setups.setup_dict[game]['general'])
+control.set_training_parameters(**setups.setup_dict[game]['training'])
 control.train()
+# control.load('/home/pgerber/Documents/RL-2018/src/DQN_Agent/models/tmp/data.chkp')
 print(control.test_Q(5, visualize=True))

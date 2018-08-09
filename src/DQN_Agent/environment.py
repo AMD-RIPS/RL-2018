5c4be7c65f3f9fa11b304f6786f1f8d04f022940
Jiajing Guan <jiajingguan@gmail.com>
2018-08-09 14:38:13 -0700

    Clip reward test, ratio 100, one tile = 1, one frame = -0.01

--- src/DQN_Agent/environment.py
+++ src/DQN_Agent/environment.py
@@ -166,7 +166,7 @@ class CarRacing:
         if reward > 0:
             clipped_reward = reward/20
         else:
-            clipped_reward = -0.005
+            clipped_reward = -0.01
         return clipped_reward
 
     def __str__(self):

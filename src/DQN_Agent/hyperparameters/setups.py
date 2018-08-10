f18f69b99957140abb071114856f6c6400589c70
Jiajing Guan <jiajingguan@gmail.com>
2018-08-09 20:45:16 -0700

    Clip reward test, ratio 100, one tile = 10, one frame = -0.1

--- src/DQN_Agent/hyperparameters/setups.py
+++ src/DQN_Agent/hyperparameters/setups.py
@@ -63,7 +63,7 @@ CarRacing = {
 		'target_update_frequency': 1000,
 		'batch_size': 32, 
 		'memory_capacity': 100000, 
-		'num_episodes': 3000,
+		'num_episodes': 9000,
 		'learning_rate_drop_frame_limit': 250000
 	}
 }

4d81b413cfe968072743e6c8eded911d2a4bd61b
phamdo <kaman@phamdo.com>
2018-08-10 17:30:13 -0700

    Add 2nd dropout architecture

--- src/DQN_Agent/architectures.py
+++ src/DQN_Agent/architectures.py
@@ -115,8 +115,27 @@ class nature_paper_dropout:
         layer3_out = tf.layers.conv2d(layer2_out, filters=64, kernel_size=[3,3],
             strides=[1,1], padding='same', activation=tf.nn.relu, data_format='channels_first', 
             kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer3_out')
-        layer4_out = tf.nn.dropout(tf.layers.dense(tf.layers.flatten(layer3_out), 512, activation=tf.nn.relu), .7, name='layer3_out')
-        output =  tf.nn.dropout(tf.layers.dense(layer4_out, action_size, activation=None), .7, name='output')
+        layer4_out = tf.nn.dropout(tf.layers.dense(tf.layers.flatten(layer3_out), 512, activation=tf.nn.relu), .7, name='layer4_out')
+        output =  tf.layers.dense(layer4_out, action_size, activation=None)
+        return output
+
+    def __str__(self):
+        return "Architecture used in the nature paper in 2015 with dropout on dense layers, 0.7 keep prob."
+
+class dropout_conv_layer:
+
+    def evaluate(self, input, action_size):
+        layer1_out = tf.layers.conv2d(input, filters=32, kernel_size=[8,8],
+            strides=[4,4], padding='same', activation=tf.nn.relu, data_format='channels_first', 
+            kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer1_out')
+        layer2_out = tf.nn.dropout(tf.layers.conv2d(layer1_out, filters=64, kernel_size=[4,4],
+            strides=[2,2], padding='same', activation=tf.nn.relu, data_format='channels_first', 
+            kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer2_out'), .7, name='layer2_out')
+        layer3_out = tf.layers.conv2d(layer2_out, filters=64, kernel_size=[3,3],
+            strides=[1,1], padding='same', activation=tf.nn.relu, data_format='channels_first', 
+            kernel_initializer=tf.contrib.layers.xavier_initializer(), name='layer3_out')
+        layer4_out = tf.layers.dense(tf.layers.flatten(layer3_out), 512, activation=tf.nn.relu)
+        output =  tf.layers.dense(layer4_out, action_size, activation=None)
         return output
 
     def __str__(self):
@@ -129,5 +148,6 @@ arch_dict = {
     'atari': atari_paper,
     'nature': nature_paper,
     'nature_batchnorm': nature_paper_batchnorm,
-    'nature_dropout': nature_paper_dropout
+    'nature_dropout': nature_paper_dropout,
+    'dropout_conv_layer': dropout_conv_layer
 }

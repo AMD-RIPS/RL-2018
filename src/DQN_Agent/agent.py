61bbd627d8498d5c5902df28df6298f3d0535f50
Patrik Gerber <pgerber@t3420n-1230bl.ipamnet.ipam.ucla.edu>
2018-08-22 12:53:51 -0700

    Added comments to agent.py and main.py

--- src/DQN_Agent/agent.py
+++ src/DQN_Agent/agent.py
@@ -15,7 +15,32 @@ from tensorflow.python.saved_model import tag_constants
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
+#####################################  Description  ####################################################
+# This file defines the class DQN_Agent. It uses Double DQN to train the neural network.
+########################################################################################################
+
 class DQN_Agent:
+
+
+	# Description: Initializes the DQN_Agent object
+	# Parameters:
+	# - environment: 		Object supporting methods like 'reset', 'render', 'step' etc.
+	# 			     		For an example see environment.py.
+	# - architecture: 		Object supporting the method 'evaluate', usually a neural network. 
+	# 				  		for exapmles see parameters/architectures.py.
+	# - explore_rate: 		Object supporting the method 'get'. See parameters/explore_rates.py for examples.
+	# - learning_rate: 		Object supporting the method 'get'. See parameters/learning_rates.py for examples.
+	# - batch_size: 		Integer giving the size of the minibatch to be used in the optimization process.
+	# - memory_capacity: 	Integer giving the size of the replay memory.
+	# - num_episodes: 		Integer specifying the number of training episodes to be used. 
+	# - learning_rate_drop_frame_limit:
+	# 						Integer specifying by which frame during training the minimal explore rate should be reached 
+	# - target_update_frequency:
+	# 						Integer specifying the frequency at which the target network's weights are to be updated
+	# - discount:			Number in (0,1) giving the discount factor
+	# - delta:  			Number, the delta parameter in huber loss
+	# - model_name:  		String, the name of the folder where the model is saved. 
+	# Output: None
 
     def __init__(self, environment, architecture, explore_rate, learning_rate,
                  batch_size, memory_capacity, num_episodes, learning_rate_drop_frame_limit,
@@ -39,6 +64,9 @@ class DQN_Agent:
         self.delta = delta
         utils.document_parameters(self)
 
+    # Description: Sets up tensorflow graph and other variables, only called internally
+    # Parameters: None
+    # Output: None
     def initialize_tf_variables(self):
         # Setting up game specific variables
         self.state_size = self.env.state_space_size
@@ -107,6 +135,10 @@ class DQN_Agent:
 
         self.sess.graph.finalize()
 
+    # Description: Performs one step of batch gradient descent on the DDQN loss function. 
+    # Parameters:
+    # - alpha: Number, the learning rate 
+    # Output: None
     def experience_replay(self, alpha):
         state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.replay_memory.get_mini_batch(self.training_metadata)
         y_batch = [None] * self.replay_memory.batch_size
@@ -122,6 +154,11 @@ class DQN_Agent:
         feed = {self.state_tf: state_batch, self.action_tf: action_batch, self.y_tf: y_batch, self.alpha: alpha}
         self.sess.run(self.train_op, feed_dict=feed)
 
+    # Description: Chooses action wrt an e-greedy policy. 
+    # Parameters:
+    # - state: 		Tensor representing a single state
+    # - epsilon: 	Number in (0,1)
+    # Output: 		Integer in the range 0...self.action_size-1 representing an action
     def get_action(self, state, epsilon):
         # Performing epsilon-greedy action selection
         if random.random() < epsilon:
@@ -129,14 +166,26 @@ class DQN_Agent:
         else:
             return self.sess.run(self.Q_argmax, feed_dict={self.state_tf: [state]})[0]
 
-    def calculate_reward(self, reward, in_grass, grass_frames):
-        if self.env.detect_grass and in_grass and grass_frames > 10:
+    # Description: Calculates the reward at a given timestep
+    # Parameters:
+    # - reward: 		Number, the reward returned by the emulator
+    # - in_grass: 		Boolean, gives wether car is in grass or not
+    # - episode_frame: 	Integer, counting the number of frames elapsed in the given episode
+    # Output: 			Number, the reward calculated
+    def calculate_reward(self, reward, in_grass, episode_frame):
+        if self.env.detect_grass and in_grass and episode_frame > 10:
             reward = -1
         return reward
 
+    # Description: Updates the weights of the target network
+    # Parameters: 	None
+    # Output: 		None
     def update_fixed_target_weights(self):
         self.fixed_target_weights = self.sess.run(self.trainable_variables)
 
+    # Description: Trains the model
+    # Parameters: 	None
+    # Output: 		None
     def train(self):
         while self.sess.run(self.episode) < self.training_metadata.num_episodes:
             episode = self.sess.run(self.episode)
@@ -152,7 +201,7 @@ class DQN_Agent:
             epsilon = self.explore_rate.get(self.training_metadata)
             alpha = self.learning_rate.get(self.training_metadata)
             print("Episode {0}/{1} \t Epsilon: {2} \t Alpha: {3}".format(episode, self.training_metadata.num_episodes, epsilon, alpha))
-            grass_frames = 0
+            episode_frame = 0
             while not done:
                 # Updating fixed target weights every #target_update_frequency frames
                 if self.training_metadata.frame % self.target_update_frequency == 0 and (self.training_metadata.frame != 0):
@@ -162,8 +211,8 @@ class DQN_Agent:
                 action = self.get_action(state, epsilon)
                 next_state, reward, done, info, in_grass = self.env.step(action)
 
-                reward = self.calculate_reward(reward, in_grass, grass_frames)
-                grass_frames += 1
+                reward = self.calculate_reward(reward, in_grass, episode_frame)
+                episode_frame += 1
 
                 self.replay_memory.add(self, state, action, reward, next_state, done)
 
@@ -181,7 +230,7 @@ class DQN_Agent:
             avg_q = self.estimate_avg_q()
 
             # Saving tensorboard data and model weights
-            if (episode % 30 == 0) and not (episode != 0):
+            if (episode % 30 == 0) and (episode != 0):
                 score, std, rewards = self.test(num_test_episodes=5, visualize=True)
                 print('{0} +- {1}'.format(score, std))
                 self.writer.add_summary(self.sess.run(self.test_summary,
@@ -190,6 +239,10 @@ class DQN_Agent:
 
             self.writer.add_summary(self.sess.run(self.training_summary, feed_dict={self.avg_q: avg_q}), episode)
 
+    # Description: Tests the model
+    # Parameters:
+    # - num_test_episodes: 	Integer, giving the number of episodes to be tested over
+    # - visualize: 			Boolean, gives whether should render the testing gameplay
     def test(self, num_test_episodes, visualize):
         rewards = []
         for episode in range(num_test_episodes):
@@ -207,10 +260,17 @@ class DQN_Agent:
             rewards.append(episode_reward)
         return np.mean(rewards), np.std(rewards), rewards
 
+    # Description: Returns average Q-value over some number of fixed tracks
+    # Parameters: 	None
+    # Output: 		None
     def estimate_avg_q(self):
         if not self.q_grid:
             return 0
         return np.average(np.amax(self.sess.run(self.Q_value, feed_dict={self.state_tf: self.q_grid}), axis=1))
 
+    # Description: Loads a model trained in a previous session
+    # Parameters:
+    # - path: 	String, giving the path to the checkpoint file to be loaded
+    # Output:	None
     def load(self, path):
         self.saver.restore(self.sess, path)
